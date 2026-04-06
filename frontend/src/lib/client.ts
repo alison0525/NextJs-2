76e62a5a85fea1c97c896a7c529065005627da30
alison0525 <ak501636@gmail.com>
2026-04-06 13:26:42 +0900

    프, 기존 fetchApi에서 credentials가 누락되는 로직을 수정

--- frontend/src/lib/client.ts
+++ frontend/src/lib/client.ts
@@ -1,13 +1,17 @@
 export function fetchApi(url: string, options?: RequestInit) {
+
+    options = options || {};
+    options.credentials = "include";
+
     if (options?.body) {
-        const header = new Headers(options.headers || {});
-        header.set("Content-Type", "application/json");
-        options.headers = header;
-        options.credentials = "include";
+        const headers = new Headers(options.headers || {});
+        headers.set("Content-Type", "application/json");
+        options.headers = headers;
     }
+
     return fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}${url}`, options).then(
         async (res) => {
-            if (!res.ok) {
+            if (!res.ok) { // 200
                 const rsData = await res.json();
                 throw new Error(rsData.msg || "요청 실패");
             }
